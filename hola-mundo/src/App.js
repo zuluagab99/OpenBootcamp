e8d687af9411ae0b111934081db1b38724248661
Alejandro Zuluaga Baron <alejo.zuluagab@gmail.com>
2022-07-03 23:27:01 -0500

    Segundo ejercicio

--- hola-mundo/src/App.js
+++ hola-mundo/src/App.js
@@ -4,6 +4,10 @@ import Greeting from './components/Pure/greeting';
 import GreetingF from './components/Pure/greetingF';
 import TaskListComponent from './components/container/task_list';
 import ContactComponent from './components/container/contact';
+import Ejemplo1 from './hooks/Ejemplo1';
+import Ejemplo2 from './hooks/Ejemplo2';
+import Clock from './components/Pure/Clock';
+import Clock2 from './components/Pure/Clock2';
 
 function App() {
   return (
@@ -14,8 +18,13 @@ function App() {
         {/* <Greeting name="Alejandro"></Greeting> */}
         {/*<GreetingF name="Alejandro"></GreetingF>*/}
         {/*<TaskListComponent></TaskListComponent>*/}
-        <ContactComponent></ContactComponent>
-
+        {/*<ContactComponent></ContactComponent>*/}
+        {/*Ejemplo de uso de Hooks*/}
+        {/*<Ejemplo1></Ejemplo1>*/}
+        {/*<Ejemplo2></Ejemplo2>*/}
+        <Clock></Clock>
+        <Clock2></Clock2>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
